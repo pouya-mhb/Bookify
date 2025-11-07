a80b77aa80e62481bf588290cc325bc25c3f79ec
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-07 15:48:48 +0330

    add admin models

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book,Cart,CartItem,Order,OrderItem,SearchHistory,UserProfile
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(SearchHistory)
+admin.site.register(UserProfile)
