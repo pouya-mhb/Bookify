8586c457b89127097b9b2e05ec35de9f7937e455
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-07 15:49:14 +0330

    save search history and books

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 import requests
 from django.conf import settings
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Book, Cart, CartItem, Order
+from .models import Book, Cart, CartItem, Order, SearchHistory
 from .serializers import BookSerializer, CartSerializer, OrderSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -28,6 +29,7 @@ def search_books(request):
 
     url = f'https://www.googleapis.com/books/v1/volumes?q={query}'
     response = requests.get(url)
+    SearchHistory.objects.create(user=request.user, query=query)
 
     if response.status_code != 200:
         return Response({'error': 'Failed to fetch data from Google Books API'}, status=response.status_code)
@@ -43,6 +45,23 @@ def search_books(request):
             'description': volume.get('description', ''),
             'published_date': volume.get('publishedDate', ''),
             'isbn': volume.get('industryIdentifiers', [{}])[0].get('identifier', ''),
+            'price' : volume.get('retailPrice', {}).get('amount', 0)
         })
+
+    # Save books in database
+    for book in books:
+        if not Book.objects.filter(isbn=book['isbn']).exists():
+            if book['published_date']:
+                published_date = book['published_date']
+                try:
+                    correct_published_date = datetime.strptime(published_date, '%Y-%m-%d').date()
+                    Book.objects.create(title=book['title'],
+                                        author=book['author'],
+                                        description=book['description'],
+                                        published_date=correct_published_date,
+                                        isbn=book['isbn'],
+                                        price=book['price'])
+                except ValueError:
+                    correct_published_date = None
 
     return Response(books, status=status.HTTP_200_OK)
