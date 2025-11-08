908917549e54145bad612c246ad7d64e7dc82aa2
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-08 21:53:46 +0330

    update serializers

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -21,28 +21,30 @@ class BookSerializer(serializers.ModelSerializer):
 
 class CartItemSerializer(serializers.ModelSerializer):
     book = BookSerializer(read_only=True)
-
+    book_id = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), source='book', write_only=True)
+    total_price = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
     class Meta:
         model = CartItem
-        fields = ['book', 'quantity']
+        fields = ['cart', 'book', 'quantity', 'book_id', 'total_price']
 
 class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(many=True, source='cartitem_set', read_only=True)
-
+    total_price = serializers.ReadOnlyField()
+    total_items = serializers.ReadOnlyField()
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'items', 'total_price']
+        fields = ['id', 'user', 'items', 'total_price', 'total_items']
 
 class OrderItemSerializer(serializers.ModelSerializer):
     book = BookSerializer(read_only=True)
 
     class Meta:
         model = OrderItem
-        fields = ['book', 'quantity']
+        fields = ['order', 'book', 'quantity', 'price']
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, source='orderitem_set', read_only=True)
 
     class Meta:
         model = Order
-        fields = ['id', 'user', 'cart', 'items', 'created_at', 'updated_at']
+        fields = ['id', 'user', 'cart', 'items', 'status', 'created_at', 'updated_at']
